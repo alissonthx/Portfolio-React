6b57fc3de144100e992166c795af43a1ceae458d
AlissonMarques-UniFCV <50666664+alissonthx@users.noreply.github.com>
2022-04-01 18:00:31 -0300

    New component About

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Intro from "./Components/intro/Intro";
 
 const App  = () => {
-  return <div><Intro/></div>;  
+  return <div><Intro/>
+              <About/>
+        </div>;  
 };
 
 export default App;
---
+++ src/Components/about/About.jsx
@@ -0,0 +1,8 @@
+import "./about.css"
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>About</div>
+  )
+}
