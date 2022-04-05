2f153a4bbb7fe08b9675f422ee52211b737de175
AlissonMarques-UniFCV <95230723+AlissonMarques-alt@users.noreply.github.com>
2022-04-05 16:57:18 -0300

    New section added to components Products and productList

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const products = [
     {
       id: 1,
-      img: "",
-      link: "#",
+      img: "https://i.postimg.cc/3xw6L3k2/alissonthx-wixsite-com.png",
+      link: "https://alissonthx.wixsite.com/gamedev",
     },    
   ];
