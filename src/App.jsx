b1df3bf9e3eb6a05b0e4b65b72fcf57c91e419e4
Alisson <alissonthx@hotmail.com>
2022-04-07 10:57:27 -0300

    add Darkmode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import About from "./Components/about/About";
 import Contact from "./Components/contact/Contact";
 import Intro from "./Components/intro/Intro";
 import ProductList from "./Components/productList/ProductList";
+import Toogle from "./Components/toogle/Toogle";
+import { ThemeContext } from "./context";
 
 const App  = () => {
-  return <div>
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return <div style={{backgroundColor: darkMode ? "rgb(14, 15, 16)" : "white", color: darkMode && "white",}}>
+    <Toogle />
     <Intro/>
     <About/>
     <ProductList/>
