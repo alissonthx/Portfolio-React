95a2fdf5ada79c1883b35e119bd5ddb3dc3a152e
alissonthx <alissonthx@hotmail.com>
2022-04-07 14:20:12 -0300

    toogle fix it

--- src/Components/toogle/Toogle.jsx
+++ src/Components/toogle/Toogle.jsx
@@ -15,7 +15,7 @@ const Toogle = () => {
     <div className="t">
         <img src={Sun} alt="" className="t-icon" />
         <img src={Moon} alt="" className="t-icon" />
-        <div className="t-button" onClick={handleClick} style={{left: state.theme.darkMode ? 0 :25}}></div>        
+        <div className="t-button" onClick={handleClick} style={{left: theme.state.darkMode ? 0 :25}}></div>        
     </div>
   )
 }
