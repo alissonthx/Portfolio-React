e2a0e1a2a1b960b9fb99bc28b6f6c651abd2312c
Álisson Marques <95230723+alissonthx@users.noreply.github.com>
2022-05-06 13:03:32 -0300

    Revert "Dev"

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export const products = [
-  {
-    id: 1,
-    img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
-    link: "https://alissonthx.github.io/javascript-learning/",
-  },    
-  {
-    id: 1,
-    img: "https://i.postimg.cc/TYv5qPHH/Countdown-Timer.png",
-    link: "https://alissonthx.github.io/javascript-learning/countdown-timer/index.html",
-  },    
-  {
-    id: 1,
-    img: "https://i.postimg.cc/7Y1GByFP/Quiz-App.png",
-    link: "https://alissonthx.github.io/javascript-learning/Quiz-App/index.html",
-  },    
-  {
-    id: 1,
-    img: "https://i.postimg.cc/3xw6L3k2/alissonthx-wixsite-com.png",
-    link: "https://alissonthx.wixsite.com/gamedev",
-  },    
+    {
+      id: 1,
+      img: "https://i.postimg.cc/3xw6L3k2/alissonthx-wixsite-com.png",
+      link: "https://alissonthx.wixsite.com/gamedev",
+    },    
+    {
+      id: 1,
+      img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
+      link: "https://alissonthx.github.io/javascript-learning/",
+    },    
+    {
+      id: 1,
+      img: "https://i.postimg.cc/TYv5qPHH/Countdown-Timer.png",
+      link: "https://alissonthx.github.io/javascript-learning/",
+    },    
+    {
+      id: 1,
+      img: "https://i.postimg.cc/7Y1GByFP/Quiz-App.png",
+      link: "https://alissonthx.github.io/javascript-learning/",
+    },    
   ];
