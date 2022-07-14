b89e5398d7ab562bbda9d92ca9370d0236752a04
Álisson Marques <95230723+alissonthx@users.noreply.github.com>
2022-07-14 18:05:38 -0300

    feat: fade in animation implementing

--- src/Components/awardList/AwardList.jsx
+++ src/Components/awardList/AwardList.jsx
@@ -1,33 +1,44 @@
 import "./awardList.css";
 import { useRef } from "react";
+import { useState } from "react";
 import { awards } from "../../data.js";
 import Award from "../award/Award";
 import Previous from "../../Assets/img/previous.svg";
 import Next from "../../Assets/img/next.svg";
 
 const AwardList = () => {
-    const carousel = useRef(null);    
 
-    const handleLeftClick = (e) => {
-        e.preventDefault();        
-        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    function fadein() {
+        const [fade, setFade] = useState(false)
+        setFade(prevState => {
+            return !prevState
+        })
     }
-    const handleRightClick = (e) => {
-        e.preventDefault();        
-        carousel.current.scrollLeft += carousel.current.offsetWidth;
-    }
+}
 
-    return (
-        <div className="a-award">
-            <img src={Previous} alt="scroll left" onClick={handleLeftClick} className="award-svg" />
-            <div className="award-list" ref={carousel}>
-                {awards.map((item) => (
-                    <Award key={item.id} img={item.img} desc={item.desc} />
-                ))}
-            </div>
-            <img src={Next} alt="scroll right" onClick={handleRightClick} className="award-svg" />
+const carousel = useRef(null);
+const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    fadein();
+}
+const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+    fadein();
+}
+
+return (
+    <div onAnimationEnd={triggerFade} className={"a-award"}{fade ? 'fadeIn' : 'visibleClass'}>
+        <img src={Previous} alt="scroll left" onClick={handleLeftClick} className="award-svg" />
+        <div className="award-list" ref={carousel}>
+            {awards.map((item) => (
+                <Award key={item.id} img={item.img} desc={item.desc} />
+            ))}
         </div>
-    );
+        <img src={Next} alt="scroll right" onClick={handleRightClick} className="award-svg" />
+    </div>
+);
 };
 
 export default AwardList;
