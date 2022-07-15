59a49af0220e3866f884a110568b06ce65b8f7e6
Álisson Marques <95230723+alissonthx@users.noreply.github.com>
2022-07-15 11:21:05 -0300

    fix: useState custom function changed

--- src/Components/awardList/AwardList.jsx
+++ src/Components/awardList/AwardList.jsx
@@ -8,37 +8,58 @@ import Next from "../../Assets/img/next.svg";
 
 const AwardList = () => {
 
-    function fadein() {
-        const [fade, setFade] = useState(false)
-        setFade(prevState => {
+    const [fadeRight, setFadeRight] = useState(false);
+    const [fadeLeft, setFadeLeft] = useState(false);
+    
+    const classN = `award-list ${fadeRight ? "fadein-next" : ""}${fadeLeft ? "fadein-previous" : ""}`;
+
+    function FadeinLeft() {
+        setFadeLeft(prevState => {
             return !prevState
         })
     }
-}
+    function FadeinRight() {
+        setFadeRight(prevState => {
+            return !prevState
+        })
+    }
 
-const carousel = useRef(null);
-const handleLeftClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft -= carousel.current.offsetWidth;
-    fadein();
-}
-const handleRightClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft += carousel.current.offsetWidth;
-    fadein();
-}
+    const carousel = useRef(null);
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        if (classN.search("fadein-previous") === -1) {
+            if (carousel.current.scrollLeft <= 0) {
+            } else {
+                carousel.current.scrollLeft -= carousel.current.offsetWidth;
+                FadeinLeft();
+                setTimeout(FadeinLeft, 1000);
+            }
+        }
+    }
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        if (classN.search("fadein-next") === -1) {
+            if (carousel.current.scrollLeft >= carousel.current.scrollWidth - carousel.current.offsetWidth) {
 
-return (
-    <div onAnimationEnd={triggerFade} className={"a-award"}{fade ? 'fadeIn' : 'visibleClass'}>
-        <img src={Previous} alt="scroll left" onClick={handleLeftClick} className="award-svg" />
-        <div className="award-list" ref={carousel}>
-            {awards.map((item) => (
-                <Award key={item.id} img={item.img} desc={item.desc} />
-            ))}
+            } else {
+                carousel.current.scrollLeft += carousel.current.offsetWidth;
+                FadeinRight();
+                setTimeout(FadeinRight, 1000);
+            }
+        }
+    }
+
+    return (
+        <div className="a-award">
+            <img src={Previous} alt="scroll left" onClick={handleLeftClick} className="award-svg" />
+            <div className={classN} ref={carousel}>
+                {awards.map((item) => (
+                    <Award key={item.id} img={item.img} desc={item.desc} />
+                ))}
+            </div>
+            <img src={Next} alt="scroll right" onClick={handleRightClick} className="award-svg" />
         </div>
-        <img src={Next} alt="scroll right" onClick={handleRightClick} className="award-svg" />
-    </div>
-);
+    );
 };
 
 export default AwardList;
