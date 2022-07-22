04299d05794cadcc4482a52423cb739c2ee70825
Álisson Marques <95230723+alissonthx@users.noreply.github.com>
2022-07-22 15:54:39 -0300

    fix: directory change

--- src/data.js
+++ src/data.js
@@ -25,27 +25,44 @@ export const products = [
     {
       id: 1,
       img: "https://i.postimg.cc/nzfhnvJg/award.jpg",
-      desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web."
+      desc: "JavaScript basic course, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
+    },
+    {
+      id: 1,
+      img: "https://i.postimg.cc/t4015xPK/Certificado-Java-Script.jpg",
+      desc: "JavaScript basic course, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
     },
     {
       id: 2,
-      img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
+      img: "https://i.postimg.cc/BQPLHg85/Certificado-PHP.jpg",
       desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
     },
     {
       id: 3,
-      img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
+      img: "https://i.postimg.cc/CxjR6xsz/Certificado-HTML.jpg",
       desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
     },
     {
       id: 4,
-      img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
+      img: "https://i.postimg.cc/vTk4C1K6/Certificado-CSS.jpg",
       desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
     },
     {
       id: 5,
-      img: "https://i.postimg.cc/y8VJtbKm/Javascript-Learning.png",
+      img: "https://i.postimg.cc/gjnwMygW/Certificado-Bootstrap-5.jpg",
       desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
+    },
+    {
+      id: 6,
+      img: "https://i.postimg.cc/9XnwDSfF/Certificado-j-Query.jpg",
+      desc: "JavaScript ES6 essencial, is a free online course for beginners learn JavaScript basics for web.",
+      link: "",
     },
   ];
   
