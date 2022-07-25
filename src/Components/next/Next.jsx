25f869df92674f7082447f99dea094dec4a28405
Álisson Marques <95230723+alissonthx@users.noreply.github.com>
2022-07-25 18:12:33 -0300

    feat: change filter in darkmode

--- src/Components/next/Next.jsx
+++ src/Components/next/Next.jsx
@@ -4,7 +4,7 @@ import right from "../../Assets/img/right.svg";
 const Next = (props) => {
 	return (
 		<div className={props.className} onClick={() => props.onClick()} >
-			<img src={right} alt="right arrow" />
+			<img style={props.style} src={right} alt="right arrow" />
 		</div>
 	)
 }
