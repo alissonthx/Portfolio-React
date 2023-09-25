1ee8ea062fb0093e356f636a9314a0072b65e617
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-09-25 14:47:59 -0300

    add: change game portfolio image

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const products = [
     {
       id: 1,
-      img: "https://i.postimg.cc/J0w72VQ7/itch.png",
+      img: "https://i.postimg.cc/yxHb2cjp/1695657880879.png",
       desc: "Here you can find my games in my Itch page.",
       link: "https://nymphdev.itch.io"
     },    
