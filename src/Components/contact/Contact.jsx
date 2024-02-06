18d51929a77ab6b409a2db0848cd060b970ce365
alissonthx <95230723+alissonthx@users.noreply.github.com>
2024-02-06 13:01:35 -0300

    add: add resume at portifolio

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import "./contact.css";
 import Phone from "../../Assets/img/phone.png";
 import Email from "../../Assets/img/email.png";
 import Address from "../../Assets/img/address.png";
+import Resume from "../../Assets/img/resume.png";
 import Github from "../../Assets/img/github.png";
 import Linkedin from "../../Assets/img/linkedin.png";
 import { useContext, useRef, useState } from "react";
@@ -19,11 +20,11 @@ const Contact = () => {
 
     emailjs.sendForm('service_2b4iajo', 'template_jowmvtq', formRef.current, 'F6Ebt5amjFFFNDvqb')
       .then((result) => {
-          console.log(result.text);
-          setDone(true);
+        console.log(result.text);
+        setDone(true);
       }, (error) => {
-          console.log(error.text);
-      });  
+        console.log(error.text);
+      });
   };
 
   return (
@@ -35,7 +36,7 @@ const Contact = () => {
           <div className="c-info">
             <div className="c-info-item">
               <img src={Phone} alt="phone icon" className="c-icon" />
-              (44) 9 9157-7785
+              +55 (44) 9821-9842
             </div>
             <div className="c-info-item">
               <img src={Email} alt="email icon" className="c-icon" />
@@ -43,13 +44,17 @@ const Contact = () => {
             </div>
             <div className="c-info-item">
               <img src={Address} alt="address icon" className="c-icon" />
-              R.Cantor Raul Seixas, 417, Jardim Ipanema, Maringá - PR
+              Maringá, Paraná, Brazil
             </div>
-            <a style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} href="https://www.linkedin.com/in/alisson-marques-miquelace/" className="c-info-item">
+            <a style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} href="https://www.linkedin.com/in/alisson-marques-miquelace/" className="c-info-item">
               <img src={Linkedin} alt="linkedin icon" className="c-icon" />
               LinkedIn
             </a>
-            <a style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} href="https://github.com/alissonthx" className="c-info-item">
+            <a style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} href="https://www.canva.com/design/DAFubOFfHlo/2VKIvDCf8I32Xxg2SLflpg/view?utm_content=DAFubOFfHlo&utm_campaign=designshare&utm_medium=link&utm_source=editor" className="c-info-item">
+              <img src={Resume} alt="Resume icon" className="c-icon" />
+              Resume
+            </a>
+            <a style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} href="https://github.com/alissonthx" className="c-info-item">
               <img src={Github} alt="github icon" className="c-icon" />
               alissonthx
             </a>
@@ -60,10 +65,10 @@ const Contact = () => {
             <b>Wanna talk with me?</b> consider the following ways to get a fresh start.
           </p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} type="text" placeholder="Name" name="user_name" />
-            <input style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} type="text" placeholder="Subject" name="user_subject" />
-            <input style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} type="text" placeholder="Email" name="user_email" />
-            <textarea style={{backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff"}} name="message" id="" rows="5"></textarea>
+            <input style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} type="text" placeholder="Name" name="user_name" />
+            <input style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} type="text" placeholder="Subject" name="user_subject" />
+            <input style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{ backgroundColor: darkMode && "rgb(14, 15, 16)", color: darkMode && "#fff" }} name="message" id="" rows="5"></textarea>
             <button>Submit</button>
             {done && <p className="c-done">Your message has been sent!</p>}
           </form>
