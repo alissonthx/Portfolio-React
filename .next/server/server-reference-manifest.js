421f1f7a3dbc99e1ae44f0473dd825248023a7cd
alissonthx <alissonthx@github.com>
2024-06-10 17:06:01 -0300

    add: build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"I16bPLZwjYJlx36IDYTzUJL5tVDYsbeI3hECP/pJ1Ds=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vdY17TRtrgRuDJWaHVQaswbhnr5TKIAwgFR0Un+jnps=\"}"
